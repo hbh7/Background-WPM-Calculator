37b954067af89b43621fc00c110e5cb5d2e1fed5
hbh7 <hbh7@hbh7.com>
2023-11-02 16:35:30 -0400

    Reformatted to a class-based system, added more design info to readme

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ A small program that watches what you type to calculate your actual average word
 
 Lots of work to still do, stay tuned!
 
+## Methodology
+- Alpha (letter) characters and timestamps are counted up until a non-alpha character is detected.
+- If the next character is a space or punctuation, these will end the word, but be counted as part of the word for WPM calculation purposes. 
+  - This is because the program is more focused on measuring sentences during normal typing, rather than testing individual word typing speeds.
+- Numbers and other special characters will end and discard any in-progress word.
+- Backspace will end and discard any in-progress word. This program (currently) does not make any attempt to measure typing accuracy. 
+
 ## Setup Info
 This script was written for Windows environments, but as long as your system can run `Python 3`, any OS will probably work just fine. You also need to install the required packages using `pip install -r requirements.txt`. Afterward, you can use Python 3 to run the script, such as via `python main.py`. 
 
--- main.py
+++ main.py
@@ -2,81 +2,114 @@ import keyboard
 import time
 import string
 
+# If you want more insight into what the program is doing with your actual key presses, or you want to debug the program,
+# turn this to True to enable debug printouts.
+debug = True
 
 ignoreKeys = ["shift"]
 additionalValidChars = ["'", "\""]
 minWordSize = 2
 maxWordSize = 16
-wordTimeoutSeconds = 5
 
-def calculateWPM(numWords, typingTime):
-    return numWords / (typingTime / 60)
+# After this many seconds without a keypress, a word's progress is reset and not added to the WPM count.
+# This was chosen with the assumption that most people will take less than 3 seconds between typing letters.
+wordTimeoutSeconds = 3
 
-def main():
+""" Wrapper function for only doing certain prints if the program is running in debug mode. """
+def dPrint(*args):
+    if debug:
+        print(args)
 
+
+class WPMCalculator:
+    # Setup instance vars
+    # Handle overall statistic tracking
     numWords = 0
     typingTime = 0
 
+    # Handle tracking word progression information
     wordLength = 0
 
     wordInProgress = False
     wordStartTime = time.time()  # Set it to some placeholder that doesn't matter but isn't None
     lastLetterTime = time.time()
 
-    while True:
-        # Wait for the next keypress event
-        event = keyboard.read_event()
+    def calculateWPM(self, numWords=None, typingTime=None):
+        # Can't set self.x variables as a default arguments, so we use these if statements as a workaround
+        if numWords is None:
+            numWords = self.numWords
+        if typingTime is None:
+            typingTime = self.typingTime
+        return numWords / (typingTime / 60)
+
+    def processEvent(self, event):
+
         if event.event_type == keyboard.KEY_DOWN:
-            print(event.to_json())
-            #print(event.time)
-            #print(event.name)
+            dPrint("New Keypress:", event.to_json())
 
             # Ignore certain keys
             if event.name in ignoreKeys:
-                continue
+                return
 
             # Reset previous word if it's been too long since the last character has been typed
             # Note that this does not mean a new word can't also start after this
-            if event.time - lastLetterTime > wordTimeoutSeconds:
-                wordInProgress = False
+            if event.time - self.lastLetterTime > wordTimeoutSeconds:
+                dPrint('Time since last typed character exceeded, resetting word progress.')
+                self.wordLength = 0
+                self.wordInProgress = False
                 # TODO: if a word was in progress, check if it should have been added to the count.
 
             # Alphanumeric character - start of word or word continuation
             if event.name in string.ascii_letters or event.name in additionalValidChars:
-                if wordInProgress:
-                    wordLength += 1
+                if self.wordInProgress:
+                    self.wordLength += 1
                 else:
-                    wordInProgress = True
-                    wordStartTime = event.time
-                    wordLength = 1
+                    self.wordInProgress = True
+                    self.wordStartTime = event.time
+                    self.wordLength = 1
 
+            # Space character
+            elif event.name == "space":
+                if self.wordInProgress:
+                    dPrint("Space detected, adding to the word length but ending the word.")
+                    self.wordLength += 1
+                    self.wordInProgress = False
+                else:
+                    dPrint("Space detected, but a word wasn't in progress, therefore ignoring the character.")
 
             # End of word or control sequence abort
-            #elif event.name == 'space' or event.name == 'space':
             else:
-                print('Non word character typed, ending word')
+                dPrint('Non word character typed, ending word')
 
                 # Discard the word if the backspace is used
                 if event.name != "backspace":
 
                     # Validate word size
-                    if minWordSize <= wordLength <= maxWordSize:
+                    if minWordSize <= self.wordLength <= maxWordSize:
                         # If valid, add to word count and typing time
-                        numWords += 1
-                        wordTime = event.time - wordStartTime
-                        typingTime += wordTime
-                        print(f'Current word length: {wordLength}, time: {wordTime}, WPM: {calculateWPM(1, wordTime)}')
-                        print(f'Current words: {numWords}, time: {typingTime}, WPM: {calculateWPM(numWords, typingTime)}')
+                        self.numWords += 1
+                        wordTime = event.time - self.wordStartTime
+                        self.typingTime += wordTime
+                        dPrint(f'Current word length: {self.wordLength}, time: {wordTime}, WPM: {self.calculateWPM(1, wordTime)}')
+                        print(f'Current words: {self.numWords}, time: {self.typingTime}, WPM: {self.calculateWPM()}')
                     else:
-                        print("Word too short, ignoring")
+                        dPrint("Word too short, ignoring")
 
                 # Reset vars
-                wordLength = 0
-                wordInProgress = False
+                self.wordLength = 0
+                self.wordInProgress = False
 
             # Adjust tracking vars
-            lastLetterTime = event.time
+            self.lastLetterTime = event.time
 
+
+def main():
+    calc = WPMCalculator()
+
+    while True:
+        # Wait for the next keypress event
+        event = keyboard.read_event()
+        calc.processEvent(event)
 
 
 if __name__ == '__main__':
